471dbbd2f7db06ff51bffa203ca70b19150f06e6
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-06-06 14:15:49 +0200

    Central vs Distributed KF example

---
+++ Matlab/MWE/functions/linear_consensus.m
@@ -0,0 +1,15 @@
+function [Fi_new, ai_new] = linear_consensus(Fi, ai, Q)
+    n_robots = numel(Fi);
+    Fi_new = cell(n_robots, 1);
+    ai_new = cell(n_robots, 1);
+
+    for i = 1:n_robots
+        Fi_new{i} = zeros(size(Fi{i}));
+        ai_new{i} = zeros(size(ai{i}));
+        for j = 1:n_robots
+            Fi_new{i} = Fi_new{i} + Fi{j};
+            ai_new{i} = ai_new{i} + ai{j};
+        end
+    end
+end
+
